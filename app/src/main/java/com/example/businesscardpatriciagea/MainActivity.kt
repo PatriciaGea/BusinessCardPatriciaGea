0996c5b246719711edb4e62bc1bd0e9ca1975af5
PatriciaGea <patricia.gea@gmail.com>
2026-05-30 22:10:43 +0200

    buttons

--- app/src/main/java/com/example/businesscardpatriciagea/MainActivity.kt
+++ app/src/main/java/com/example/businesscardpatriciagea/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.businesscardpatriciagea
 
 import android.content.Intent
-import android.net.Uri
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -24,7 +25,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.businesscardpatriciagea.ui.theme.BusinessCardPatriciaGeaTheme
 import androidx.compose.ui.graphics.Color
-
+import androidx.compose.ui.layout.ContentScale
+import androidx.core.net.toUri
 
 
 class MainActivity : ComponentActivity() {
@@ -55,7 +57,7 @@ fun BusinessCardScreen(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Spacer(modifier = Modifier.height(120.dp))
+        Spacer(modifier = Modifier.height(50.dp))
 
         Text(
             "Patricia Gea",
@@ -88,35 +90,46 @@ fun BusinessCardScreen(modifier: Modifier = Modifier) {
             contentDescription = "Minha foto",
             modifier = Modifier
                 .size(260.dp)
+                .background(Color.White, CircleShape)
                 .clip(CircleShape)
-                .border(1.dp, Color.Black, CircleShape)
-        )
-
+                .border(1.dp, Color.Black, CircleShape))
         Spacer(modifier = Modifier.height(30.dp))
+        Image(
+            painter = painterResource(R.drawable.androidvector),
+            contentDescription = "Android",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.width(150.dp)
+        )
+        Spacer(modifier = Modifier.height(5.dp))
 
             ContactCard()
     }
 }
 
-data class LinkItem(
-    val title: String,
-    val url: String
-)
+//data class LinkItem(
+//    val title: String,
+//    val url: String
+//)
 
 
 @Composable
 fun ContactCard() {
 
-    val context = LocalContext.current
-
-    val links = listOf(
-        LinkItem("Email me", "mailto:patricia@email.com"),
-        LinkItem("LinkedIn", "https://linkedin.com"),
-        LinkItem("GitHub", "https://github.com")
-    )
+//    val context = LocalContext.current
+//
+//    val links = listOf(
+//        LinkItem("Email", "mailto:patricia.gea@email.com"),
+//        LinkItem("patriciageadev.vercel.app", "https://patriciageadev.vercel.app/"),
+//        LinkItem("GitHub", "https://github.com")
+//    )
 
     Card(
-        modifier = Modifier.padding(16.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .size(
+                width = 260.dp,
+                height = 350.dp
+            ),
         shape = RoundedCornerShape(20.dp),
         border = BorderStroke(
             1.dp,
@@ -125,23 +138,41 @@ fun ContactCard() {
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface
         )
-    ) {
+    )
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { }) {
+                Text("Email")
+            }
 
+            Spacer(modifier = Modifier.width(16.dp)) // 👈 AQUI O ESPAÇO
+
+            Button(onClick = { }) {
+                Text("Call")
+            }
+        }
         Column(
-            modifier = Modifier.padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp)
+            modifier = Modifier
+                .fillMaxSize()
+
         ) {
 
-            links.forEach { item ->
-                Text(
-                    text = item.title,
-                    modifier = Modifier.clickable {
-                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(item.url))
-                        context.startActivity(intent)
-                    },
-                    style = MaterialTheme.typography.bodyLarge
-                )
-            }
+//            links.forEach { item ->
+//                Text(
+//                    text = item.title,
+//                    modifier = Modifier.clickable {
+//                        val intent = Intent(Intent.ACTION_VIEW, item.url.toUri())
+//                        context.startActivity(intent)
+//                    },
+//                    style = MaterialTheme.typography.bodyLarge
+//                )
+//            }
         }
     }
 }
